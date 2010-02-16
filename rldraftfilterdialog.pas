1fa74feb1143ec45b9f667b629704e53d19efcb7
blikblum <blikblum@1a04b66b-1225-4157-9a46-5a25c0b778ad>
2010-02-16 21:53:00 +0000

    * Limpado código do RLDraftFilterDialog

--- rldraftfilterdialog.pas
+++ rldraftfilterdialog.pas
@@ -1,27 +1,24 @@
-{$I RLReport.inc}
 {@unit RLDraftFilterDialog
 Portado para o Lazarus - Trabalho inicial de Isaac Trindade da Silva contato tioneobrasil@yahoo.com.br dicas4lazarus@yahoo.com.br
 Lazarus Ported - initial work by Isaac 07/2009
 }
 unit RLDraftFilterDialog;
+
 {$MODE DELPHI}
+{$I RLReport.inc}
+
 interface
 
 uses
   SysUtils, Classes,
-{$ifdef VCL}
-  {$IFDEF FPC}
-  LCLIntf, LCLType,
-  {$ELSE}
-  Windows,
-  {$ENDIF}
+  LCLIntf, LCLType, LResources,
   Controls, Buttons, ExtCtrls, Forms, Dialogs, StdCtrls, Graphics,
-{$else}
-  Types, QControls, Qt, QButtons, QExtCtrls, QForms, QDialogs, QStdCtrls, QTypes, QGraphics,
-{$endif}
   RLConsts, RLDraftFilter;
 
 type
+
+  { TRLDraftFilterDialogForm }
+
   TRLDraftFilterDialogForm = class(TForm)
     ButtonOk: TButton;
     ButtonCancel: TButton;
@@ -29,7 +26,6 @@ type
   private
     { Private declarations }
     Filter:TRLDraftFilter;
-    procedure Init;
   public
     { Public declarations }
     class function Execute(Filter:TRLDraftFilter):boolean;
@@ -40,8 +36,6 @@ var
 
 implementation
 
-///{$R *.dfm}
-
 { TRLDraftFilterDialogForm }
 
 class function TRLDraftFilterDialogForm.Execute(
@@ -49,13 +43,15 @@ class function TRLDraftFilterDialogForm.Execute(
 var
   Form:TRLDraftFilterDialogForm;
 begin
-  {$IFDEF FPC}
-  Form:=TRLDraftFilterDialogForm.CreateNew(nil,0);
-  {$ELSE}
-  Form:=TRLDraftFilterDialogForm.CreateNew(nil);
-  {$ENDIF}
+  Form := TRLDraftFilterDialogForm.Create(nil);
   try
-    Form.Init;
+    Form.ButtonOk.Caption := LS_OkStr;
+    with Form.RadioGroupFormulary do
+    begin
+      Items.Add('80 '+LS_ColumnsStr);
+      Items.Add('132 '+LS_ColumnsStr);
+    end;
+
     Form.Filter:=Filter;
     case Filter.FormSelection of
       fs80Cols : Form.RadioGroupFormulary.ItemIndex:=0;
@@ -70,74 +66,10 @@ begin
         1: Filter.FormSelection:=fs132Cols;
       end;
   finally
-    Form.Free;
+    Destroy;
   end;
 end;
 
-procedure TRLDraftFilterDialogForm.Init;
-begin
-  Left := 512;
-  Top := 475;
-{$ifdef VCL}
-  BorderStyle := bsDialog
-{$else}
-  BorderStyle := fbsDialog;
-{$endif};
-  Caption := LS_OptionsStr;
-  ClientHeight := 122;
-  ClientWidth := 262;
-  Color := clBtnFace;
-  Font.Charset := DEFAULT_CHARSET;
-  Font.Color := clWindowText;
-  Font.Height := -11;
-  Font.Name := 'MS Sans Serif';
-  Font.Style := [];
-  Position := poScreenCenter;
-  PixelsPerInch := 96;
-  ButtonOk:=TButton.Create(Self);
-  with ButtonOk do
-  begin
-    Parent := Self;
-    Name := 'ButtonOk';
-    Left := 92;
-    Top := 88;
-    Width := 75;
-    Height := 25;
-    Caption := LS_OkStr;
-    Default := True;
-    ModalResult := 1;
-    TabOrder := 0;
-  end;
-  ButtonCancel:=TButton.Create(Self);
-  with ButtonCancel do
-  begin
-    Parent := Self;
-    Name := 'ButtonCancel';
-    Left := 176;
-    Top := 88;
-    Width := 75;
-    Height := 25;
-    Cancel := True;
-    Caption := LS_CancelStr;
-    ModalResult := 2;
-    TabOrder := 1;
-  end;
-  RadioGroupFormulary:=TRadioGroup.Create(Self);
-  with RadioGroupFormulary do
-  begin
-    Parent := Self;
-    Name := 'RadioGroupFormulary';
-    Left := 8;
-    Top := 8;
-    Width := 245;
-    Height := 65;
-    Caption := ' '+LS_FormStr+': ';
-    TabOrder := 2;
-    Items.Clear;
-    Items.Add('80 '+LS_ColumnsStr);
-    Items.Add('132 '+LS_ColumnsStr);
-    ItemIndex:= 0;
-  end;
-end;
-
+initialization
+  {$i rldraftfilterdialog.lrs}
 end.
