47efb52cad7b7f531d13f67851675ab77adb7daa
blikblum <blikblum@1a04b66b-1225-4157-9a46-5a25c0b778ad>
2010-05-04 17:57:28 +0000

    * Limpado código / Melhorado layout do RLPageSetupConfig

--- rlpagesetupconfig.pas
+++ rlpagesetupconfig.pas
@@ -9,35 +9,27 @@ unit RLPageSetupConfig;
 interface
 
 uses
-  Classes, SysUtils,
-{$IFDEF FPC}
-  LCLIntf,
- {$ifdef MSWINDOWS}
-//   Windows,
- {$else}
-//   LCLIntf,
- {$endif}
- LCLType,
-{$ELSE}
- {$ifdef MSWINDOWS}
-   Windows,
- {$endif}
-{$ENDIF}
-
-{$ifdef VCL}
-  Messages, Graphics, Controls, Forms, Dialogs, StdCtrls,
-{$else}
-  QGraphics, QControls, QForms, QDialogs, QStdCtrls,
-{$endif}
+  Classes, SysUtils, LCLType, Graphics, Controls, Forms, Dialogs, StdCtrls, LResources,
   RLFilters, RLConsts, RLPrinters, RLTypes, RLSpoolFilter;
 
 type
 
+  { TRLPageSetupConfig }
+
   TRLPageSetupConfig = class(TForm)
-    GroupBoxPages: TGroupBox;
-    GroupBoxCopies: TGroupBox;
     ButtonAplicar: TButton;
+    ComboBoxPaperOrientation: TComboBox;
+    ComboBoxSetPaper: TComboBox;
+    EditPaperHeight: TEdit;
+    EditPaperWidth: TEdit;
+    EditMarginsLeft: TEdit;
+    GroupBoxPaper: TGroupBox;
+    GroupBoxPageMargins: TGroupBox;
     ButtonCancel: TButton;
+    LabelPaperHeight: TLabel;
+    LabelPaperWidth: TLabel;
+    LabelSetPaper: TLabel;
+    LabelPaperOrientation: TLabel;
     RadioButtonPagesAll: TRadioButton;
     RadioButtonPagesInterval: TRadioButton;
     RadioButtonPagesSelect: TRadioButton;
@@ -47,30 +39,18 @@ type
     LabelMarginsLeft: Tlabel;
     LabelMarginsRigth: Tlabel;            
     EditMarginsBottom: TEdit;
-    EditMarginsLeft: TEdit;
     EditMarginsRigth: TEdit;
-    LabelSetPaper: TLabel;
-    LabelPaperWidth: TLabel;
-    EditPaperWidth: Tedit;
-    LabelPaperHeight: Tlabel;
-    EditPaperHeight: Tedit;              
-    ComboBoxSetPaper: TComboBox;
-    ComboBoxPaperOrientation: TComboBox;
-    LabelPaperOrientation: Tlabel;    
-    ButtonOptions: TButton;
-    SpeedButtonok: Tbutton;
     procedure EditFromToPageChange(Sender: TObject);
     procedure ComboBoxSetPaperChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
     procedure LoadEditors;
     procedure SaveEditors;
-    procedure Init;
   protected
     { Protected declarations }
   public
     { Public declarations }
-    constructor Create(aOwner:TComponent); override;
     function Execute:boolean;
   end;
 
@@ -121,8 +101,6 @@ var PageParams:TRLPageParams=nil;
 
 implementation
 
-///{$R *.dfm}
-
 // UTILS
 
 function IntToEmptyStr(aInt:integer):string;
@@ -140,311 +118,15 @@ end;
 
 { TRLPrintDialog }
 
-// OVERRIDE
-
-constructor TRLPageSetupConfig.Create(aOwner:TComponent);
-begin
-  {$IFDEF FPC}
-  inherited CreateNew(aOwner,0);
-  {$ELSE}
-  inherited CreateNew(aOwner);
-  {$ENDIF}
-  //
-  Init;
-end;
-
 // PUBLIC
 
 function TRLPageSetupConfig.Execute:boolean;
 begin
   LoadEditors;
-  ActiveControl:=GroupBoxPages;
+  ActiveControl:=GroupBoxPageMargins;
   Result:=(ShowModal=mrOk);
   if Result then
     SaveEditors;
-end;
-
-// PRIVATE
-
-procedure TRLPageSetupConfig.Init;
-begin
-  Left := 324;
-  Top := 372;
-{$ifdef VCL}
-  BorderStyle := bsDialog
-{$else}
-  BorderStyle := fbsDialog;
-{$endif};
-  Caption := Ls_Page_Setings;
-  ClientHeight := 150;
-  ClientWidth := 430;
-  Color := clBtnFace;
-  Font.Charset := DEFAULT_CHARSET;
-  Font.Color := clWindowText;
-  Font.Height := -11;
-  Font.Name := 'MS Sans Serif';
-  Font.Style := [];
-  Position := poScreenCenter;
-  {$IFNDEF FPC}
-  Scaled := False;
-  {$ENDIF}
-  PixelsPerInch := 96;
-  GroupBoxPages:=TGroupBox.Create(Self);
-  with GroupBoxPages do
-  begin
-    Name := 'GroupBoxPages';
-    Parent := Self;
-    Left := 8;
-    Top := 10;
-    Width := 217;
-    Height := 130;
-    Caption := ' ' + Ls_Page_margins + ' ';
-    TabOrder := 1;
-    LabelMarginsTop:=TLabel.Create(Self);
-    with LabelMarginsTop do
-    begin
-      Name := 'LabelMarginsTop';
-      Parent := GroupBoxPages;
-      Left := 10;
-      Top := 49;
-      Width := 18;
-      Height := 13;
-      Caption := '&'+Ls_Page_margins_top;
-      FocusControl := EditMarginsBottom;
-    end;
-    LabelMarginsBottom:=TLabel.Create(Self);
-    with LabelMarginsBottom do
-    begin
-      Name := 'LabelMarginsBottom';
-      Parent := GroupBoxPages;
-      Left := 10;
-      Top := 20;
-      Width := 18;
-      Height := 13;
-      Caption := '&'+Ls_Page_margins_bottom;
-      FocusControl := EditMarginsBottom;
-    end;
-    LabelMarginsLeft:=TLabel.Create(Self);
-    with LabelMarginsLeft do
-    begin
-      Name := 'LabelMarginsLeft';
-      Parent := GroupBoxPages;
-      Left := 110;
-      Top := 49;
-      Width := 18;
-      Height := 13;
-      Caption := '&'+Ls_Page_margins_left;
-      FocusControl := EditMarginsBottom;
-    end;
-    LabelMarginsRigth:=TLabel.Create(Self);
-    with LabelMarginsRigth do
-    begin
-      Name := 'LabelMarginsRigth';
-      Parent := GroupBoxPages;
-      Left := 110;
-      Top := 20;
-      Width := 18;
-      Height := 13;
-      Caption := '&'+Ls_Page_margins_rigth;
-      FocusControl := EditMarginsBottom;
-    end;
-    EditMarginsTop:=TEdit.Create(Self);
-    with EditMarginsTop do
-    begin
-      Name := 'EditMarginsTop';
-      Parent := GroupBoxPages;
-      Left := 60;
-      Top := 15;
-      Width := 41;
-      Height := 21;
-      TabOrder := 2;
-      Text := ' ';
-      OnChange := EditFromToPageChange;
-    end;
-    EditMarginsBottom:=TEdit.Create(Self);
-    with EditMarginsBottom do
-    begin
-      Name := 'EditMarginsBottom';
-      Parent := GroupBoxPages;
-      Left := 60;
-      Top := 45;
-      Width := 41;
-      Height := 21;
-      TabOrder := 3;
-      OnChange := EditFromToPageChange;
-    end;
-    EditMarginsLeft:=TEdit.Create(Self);
-    with EditMarginsLeft do
-    begin
-      Name := 'EditMarginsLeft';
-      Parent := GroupBoxPages;
-      Left := 165;
-      Top := 45;
-      Width := 41;
-      Height := 21;
-      TabOrder := 3;
-      Text := '';
-      OnChange := EditFromToPageChange;
-    end;
-    EditMarginsRigth:=TEdit.Create(Self);
-    with EditMarginsRigth do
-    begin
-      Name := 'EditMarginsRigth';
-      Parent := GroupBoxPages;
-      Left := 165;
-      Top := 15;
-      Width := 41;
-      Height := 21;
-      TabOrder := 3;
-      Text := '';
-      OnChange := EditFromToPageChange;
-    end;
-    ComboBoxPaperOrientation:=TComboBox.Create(Self);
-    with ComboBoxPaperOrientation do
-    begin
-      Name := 'ComboBoxPaperOrientation';
-      Parent := GroupBoxPages;
-      Left := 10;
-      Top := 100;
-      Width := 198;
-      Height :=  21;
-      Style := csDropDownList;
-      ItemHeight  := 13;
-      TabOrder :=  0;
-      items.Add (Ls_Paper_Orientation_Portrait);
-      items.Add (Ls_Paper_Orientation_Landscape);
-      ItemIndex:=0;
-      OnChange := ComboBoxSetPaperChange;
-    end;
-    LabelPaperOrientation:=TLabel.Create(Self);
-    with LabelPaperOrientation do
-    begin
-      Name := 'LabelPaperOrientation';
-      Parent := GroupBoxPages;
-      Left := 10;
-      Top := 85;
-      Width := 18;
-      Height := 13;
-      Caption := '&'+Ls_Paper_Orientation;
-      FocusControl := ComboBoxPaperOrientation;
-    end;
-  end;
-  GroupBoxCopies:=TGroupBox.Create(Self);
-  with GroupBoxCopies do
-  begin
-    Name := 'GroupBoxCopies';
-    Parent := Self;
-    Left := 236;
-    Top := 10;
-    Width := 185;
-    Height := 101;
-    Caption := ' ' + Ls_Page_Paper + ' ';
-    TabOrder := 2;
-    LabelSetPaper:=TLabel.Create(Self);
-    with LabelSetPaper do
-    begin
-      Name := 'LabelSetPaper';
-      Parent := GroupBoxCopies;
-      Left := 10;
-      Top := 15;
-      Width := 15;
-      Height := 13;
-      Caption := Ls_Paper_Size;
-      FocusControl := ComboBoxSetPaper;
-    end;
-    ComboBoxSetPaper:=TComboBox.Create(Self);
-    with ComboBoxSetPaper do
-    begin
-      Name := 'ComboBoxSetPaper';
-      Parent := GroupBoxCopies;
-      Left := 10;
-      Top := 30;
-      Width := 165;
-      Height := 21;
-      Style := csDropDownList;
-      ItemHeight := 13;
-      TabOrder := 0;
-      OnChange := ComboBoxSetPaperChange;
-    end;
-    LabelPaperWidth:=TLabel.Create(Self);
-    with LabelPaperWidth do
-    begin
-      Name := 'LabelPaperWidth';
-      Parent := GroupBoxCopies;
-      Left := 10;
-      Top := 55;
-      Width := 15;
-      Height := 13;
-      Caption := '&'+Ls_Paper_Size_Width;
-      FocusControl := GroupBoxCopies;
-    end;
-    EditPaperWidth:=TEdit.Create(Self);
-    with EditPaperWidth do
-    begin
-      Name := 'EditPaperWidth';
-      Parent := GroupBoxCopies;
-      Left := 10;
-      Top := 70;
-      Width := 50;
-      Height := 21;
-      TabOrder := 3;
-      Text := '';
-      OnChange := EditFromToPageChange;
-    end;
-    LabelPaperHeight:=TLabel.Create(Self);
-    with LabelPaperHeight do
-    begin
-      Name := 'LabelPaperHeight';
-      Parent := GroupBoxCopies;
-      Left := 80;
-      Top := 55;
-      Width := 20;
-      Height := 13;
-      Caption := '&'+Ls_Paper_Size_Width;
-      FocusControl := EditPaperHeight;
-    end;
-    EditPaperHeight:=TEdit.Create(Self);
-    with EditPaperHeight do
-    begin
-      Name := 'EditPaperHeight';
-      Parent := GroupBoxCopies;
-      Left := 80;
-      Top := 70;
-      Width := 50;
-      Height := 21;
-      TabOrder := 3;
-      Text := '';
-      OnChange := EditFromToPageChange;
-    end;
-  end;
-  ButtonAplicar:=TButton.Create(Self);
-  with ButtonAplicar do
-  begin
-    Name := 'ButtonAplicar';
-    Parent := Self;
-    Left := 260;
-    Top := 115;
-    Width := 75;
-    Height := 25;
-    Caption := Ls_Aplicar;
-    ModalResult := 1;
-    TabOrder := 3;
-  end;
-  ButtonCancel:=TButton.Create(Self);
-  with ButtonCancel do
-  begin
-    Name := 'ButtonCancel';
-    Parent := Self;
-    Left := 345;
-    Top := 115;
-    Width := 75;
-    Height := 25;
-    Cancel := True;
-    Caption := LS_CancelStr;
-    ModalResult := 2;
-    TabOrder := 4;
-  end;
-  //
 end;
 
 
@@ -491,7 +173,7 @@ end;
 
 procedure TRLPageSetupConfig.EditFromToPageChange(Sender: TObject);
 begin
-
+  //todo: implementar
 end;
 
 procedure TRLPageSetupConfig.ComboBoxSetPaperChange(Sender: TObject);
@@ -502,14 +184,32 @@ begin
   PaperInfo[PaperSize].Width:= 0; ///?
 end;
 
+procedure TRLPageSetupConfig.FormCreate(Sender: TObject);
+begin
+  Caption := Ls_Page_Settings;
+  GroupBoxPageMargins.Caption := Ls_Page_margins;
+  LabelMarginsTop.Caption := '&'+Ls_Page_margins_top;
+  LabelMarginsBottom.Caption := '&'+Ls_Page_margins_bottom;
+  LabelMarginsLeft.Caption := '&'+Ls_Page_margins_left;
+  LabelMarginsRigth.Caption := '&'+Ls_Page_margins_rigth;
+  LabelPaperOrientation.Caption := '&'+Ls_Paper_Orientation;
+  GroupBoxPaper.Caption := Ls_Page_Paper;
+  LabelSetPaper.Caption := Ls_Paper_Size;
+  LabelPaperWidth.Caption := '&'+Ls_Paper_Size_Width;
+  LabelPaperHeight.Caption := '&'+Ls_Paper_Size_Heigth;
+  ButtonAplicar.Caption := Ls_Aplicar;
+  ButtonCancel.Caption := LS_CancelStr;
+  ComboBoxPaperOrientation.Items.Add(Ls_Paper_Orientation_Portrait);
+  ComboBoxPaperOrientation.Items.Add(Ls_Paper_Orientation_Landscape);
+  ComboBoxPaperOrientation.ItemIndex := 0;
+end;
+
 { TRLPageParams }
 
 constructor TRLPageParams.Create(aOwner: TComponent);
 begin
   //
   inherited;
-
-
 end;
 
 destructor TRLPageParams.Destroy;
@@ -554,6 +254,7 @@ fPageOrientation:=Value;
 end;
 
 initialization
+  {$i rlpagesetupconfig.lrs}
   PageParams:=TRLPageParams.Create(nil);
 
 finalization
